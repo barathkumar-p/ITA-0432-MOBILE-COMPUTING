0597fd4f540222433ad150571ee2a31b74db6b13
barathkumar-p <81065407+barathkumar-p@users.noreply.github.com>
2023-02-06 22:07:00 +0530

    Create webview.java

---
+++ webview.java
@@ -0,0 +1,30 @@
+package com.example.app9webview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
